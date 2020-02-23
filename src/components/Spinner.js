8897c49406d327ed5f8764b65285f8bef1751259
Marcin Olszewski <marcin@Marcins-MacBook-Pro.local>
2020-02-23 21:00:58 +0100

    [fix] removed unnecessary files

--- src/components/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Spinner() {
-  return <div></div>;
-}
-
-export default Spinner;
--- src/components/ThankYouPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ThankYouPage() {
-  return <div></div>;
-}
-
-export default ThankYouPage;
