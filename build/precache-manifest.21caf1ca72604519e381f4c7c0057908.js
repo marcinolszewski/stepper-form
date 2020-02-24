abcf6a960867190e549a7505fe11e4515fe4f3ab
Marcin Olszewski <marcin@Marcins-MacBook-Pro.local>
2020-02-24 18:07:41 +0100

    [feat] added dev

---
+++ build/precache-manifest.21caf1ca72604519e381f4c7c0057908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ac1a9cc9a798e6548eb1e045672954",
+    "url": "/stepper-form/index.html"
+  },
+  {
+    "revision": "67bc67dbf86a16538cb3",
+    "url": "/stepper-form/static/css/main.196c4980.chunk.css"
+  },
+  {
+    "revision": "5693561b4135ca909d62",
+    "url": "/stepper-form/static/js/2.05235a8b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/stepper-form/static/js/2.05235a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bc67dbf86a16538cb3",
+    "url": "/stepper-form/static/js/main.fa4109bd.chunk.js"
+  },
+  {
+    "revision": "3ab4c6474cfc1339bd7d",
+    "url": "/stepper-form/static/js/runtime-main.d94a1b31.js"
+  }
+]);
